91dbc14aba2e7e0dffb9272a31f2af9a48f13453
bhaveshsinghal95182 <168349039+bhaveshsinghal95182@users.noreply.github.com>
2024-05-13 01:38:39 +0530

    compeleted the whole project

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+ALIGNMENT = "left"
 
+class Scoreboard(Turtle):
 
-class Scoreboard:
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 260)
+        self.update_scoreboard()
+
+    def increasse_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"level: {self.level}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align="center", font=FONT)
